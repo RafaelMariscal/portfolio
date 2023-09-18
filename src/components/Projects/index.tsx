d82dd122ba774a7b57a4129f927855e5e89c7d2e
Rafael Mariscal <rafael_mariscal_@outlook.com>
2023-09-18 09:10:26 -0300

    Refactor: Projects component quick import refactor

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,72 +1,9 @@
 import JackePlanner from '@/assets/projects/jp-project.png'
 import RDProject from '@/assets/projects/rd-project.png'
 import IBProject from '@/assets/projects/ib-project.png'
-import javascript from '@/assets/icons/techs/JAVASCRIPT.svg'
-import html from '@/assets/icons/techs/HTML.svg'
-import css from '@/assets/icons/techs/CSS.svg'
-import typescript from '@/assets/icons/techs/TYPESCRIPT.svg'
-import react from '@/assets/icons/techs/REACT.svg'
-import radix from '@/assets/icons/techs/RADIX.svg'
-import router from '@/assets/icons/techs/ROUTER.svg'
-import tailwind from '@/assets/icons/techs/TAILWIND.svg'
-import firebase from '@/assets/icons/techs/FIREBASE.svg'
-import nextjs from '@/assets/icons/techs/NEXT.svg'
-import nestjs from '@/assets/icons/techs/NEST.svg'
-import nodejs from '@/assets/icons/techs/NODE.svg'
-import mongoDB from '@/assets/icons/techs/MONGO.svg'
-import prisma from '@/assets/icons/techs/PRISMA.svg'
-import docker from '@/assets/icons/techs/DOCKER.svg'
-import framer from '@/assets/icons/techs/FRAMER_MOTION.svg'
-import reactHookForm from '@/assets/icons/techs/REACT_HOOK_FORM.svg'
-import zod from '@/assets/icons/techs/ZOD.svg'
-import aws from '@/assets/icons/techs/AWS.svg'
-import { StaticImageData } from 'next/image'
-import { ProjectCompose } from './ProjectCompose'
 import NavLink from '../Navigation/NavLink'
-
-const rdInspecTechs: StaticImageData[] = [
-  html,
-  css,
-  javascript,
-  typescript,
-  react,
-  nextjs,
-  radix,
-  tailwind,
-  nodejs,
-  nestjs,
-  prisma,
-  docker,
-  mongoDB,
-  aws,
-]
-
-const jackedPlannerTechs: StaticImageData[] = [
-  html,
-  css,
-  javascript,
-  typescript,
-  react,
-  radix,
-  router,
-  tailwind,
-  firebase,
-]
-
-const IbWebsiteTechs: StaticImageData[] = [
-  html,
-  css,
-  javascript,
-  typescript,
-  react,
-  nextjs,
-  tailwind,
-  radix,
-  framer,
-  reactHookForm,
-  zod,
-  nodejs,
-]
+import { ProjectCompose } from './ProjectCompose'
+import { rdInspecTechs, jackedPlannerTechs, IbWebsiteTechs } from './techsLists'
 
 export default function Projects() {
   return (
---
+++ src/components/Projects/techsLists.ts
@@ -0,0 +1,64 @@
+import { StaticImageData } from 'next/image'
+import javascript from '@/assets/icons/techs/JAVASCRIPT.svg'
+import html from '@/assets/icons/techs/HTML.svg'
+import css from '@/assets/icons/techs/CSS.svg'
+import typescript from '@/assets/icons/techs/TYPESCRIPT.svg'
+import react from '@/assets/icons/techs/REACT.svg'
+import radix from '@/assets/icons/techs/RADIX.svg'
+import router from '@/assets/icons/techs/ROUTER.svg'
+import tailwind from '@/assets/icons/techs/TAILWIND.svg'
+import firebase from '@/assets/icons/techs/FIREBASE.svg'
+import nextjs from '@/assets/icons/techs/NEXT.svg'
+import nestjs from '@/assets/icons/techs/NEST.svg'
+import nodejs from '@/assets/icons/techs/NODE.svg'
+import mongoDB from '@/assets/icons/techs/MONGO.svg'
+import prisma from '@/assets/icons/techs/PRISMA.svg'
+import docker from '@/assets/icons/techs/DOCKER.svg'
+import framer from '@/assets/icons/techs/FRAMER_MOTION.svg'
+import reactHookForm from '@/assets/icons/techs/REACT_HOOK_FORM.svg'
+import zod from '@/assets/icons/techs/ZOD.svg'
+import aws from '@/assets/icons/techs/AWS.svg'
+
+export const rdInspecTechs: StaticImageData[] = [
+  html,
+  css,
+  javascript,
+  typescript,
+  react,
+  nextjs,
+  radix,
+  tailwind,
+  nodejs,
+  nestjs,
+  prisma,
+  docker,
+  mongoDB,
+  aws,
+]
+
+export const jackedPlannerTechs: StaticImageData[] = [
+  html,
+  css,
+  javascript,
+  typescript,
+  react,
+  radix,
+  router,
+  tailwind,
+  firebase,
+]
+
+export const IbWebsiteTechs: StaticImageData[] = [
+  html,
+  css,
+  javascript,
+  typescript,
+  react,
+  nextjs,
+  tailwind,
+  radix,
+  framer,
+  reactHookForm,
+  zod,
+  nodejs,
+]
