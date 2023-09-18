3f18d05f11da9e8fb383ebe68209d11bed02a349
Rafael Mariscal <rafael_mariscal_@outlook.com>
2023-09-18 09:04:46 -0300

    Refactor: Bootcamp projects component quick import refactor

--- src/components/BootcampsProjects/index.tsx
+++ src/components/BootcampsProjects/index.tsx
@@ -1,82 +1,18 @@
-import Image, { StaticImageData } from 'next/image'
+import Image from 'next/image'
 import WaiterApp from '@/assets/projects/bootcampProjects/waiterApp.png'
 import DesignSystem from '@/assets/projects/bootcampProjects/designSystem.png'
 import NextBlog from '@/assets/projects/bootcampProjects/nextjsBlog.png'
 import VideoPlatform from '@/assets/projects/bootcampProjects/videoPlatform.png'
-import { BootcampProject } from './BootcampProject'
-
 import Squares from '@/assets/Squares2.svg'
-import html from '@/assets/icons/techs/HTML.svg'
-import css from '@/assets/icons/techs/CSS.svg'
-import sass from '@/assets/icons/techs/SASS.svg'
-import javascript from '@/assets/icons/techs/JAVASCRIPT.svg'
-import typescript from '@/assets/icons/techs/TYPESCRIPT.svg'
-import react from '@/assets/icons/techs/REACT.svg'
-import reactNative from '@/assets/icons/techs/REACTNATIVE.svg'
-import storybook from '@/assets/icons/techs/STORYBOOK.svg'
-import radix from '@/assets/icons/techs/RADIX.svg'
-import tailwind from '@/assets/icons/techs/TAILWIND.svg'
-import nextjs from '@/assets/icons/techs/NEXT.svg'
-import nodejs from '@/assets/icons/techs/NODE.svg'
-import express from '@/assets/icons/techs/EXPRESS.svg'
-import mongoDB from '@/assets/icons/techs/MONGO.svg'
-import prisma from '@/assets/icons/techs/PRISMA.svg'
-import docker from '@/assets/icons/techs/DOCKER.svg'
-import jest from '@/assets/icons/techs/JEST.svg'
-import graphQL from '@/assets/icons/techs/GRAPHQL.svg'
-import graphCMS from '@/assets/icons/techs/GRAPH_CMS.svg'
-import apollo from '@/assets/icons/techs/APOLLO.svg'
 import jstackIcon from '@/assets/icons/bootcampCompanies/JStack.svg'
 import rocketseatIcon from '@/assets/icons/bootcampCompanies/Rocketseat.svg'
-
-const waiterAppTechs: StaticImageData[] = [
-  html,
-  css,
-  javascript,
-  typescript,
-  react,
-  reactNative,
-  docker,
-  nodejs,
-  express,
-  mongoDB,
-]
-
-const designSystemTechs: StaticImageData[] = [
-  html,
-  css,
-  javascript,
-  typescript,
-  react,
-  radix,
-  storybook,
-  jest,
-]
-
-const nextBlogTechs: StaticImageData[] = [
-  html,
-  css,
-  sass,
-  javascript,
-  typescript,
-  react,
-  nextjs,
-  jest,
-  nodejs,
-  prisma,
-]
-
-const videoPlatformTechs: StaticImageData[] = [
-  html,
-  css,
-  javascript,
-  typescript,
-  react,
-  tailwind,
-  graphQL,
-  graphCMS,
-  apollo,
-]
+import { BootcampProject } from './BootcampProject'
+import {
+  nextBlogTechs,
+  waiterAppTechs,
+  designSystemTechs,
+  videoPlatformTechs,
+} from './techsLists'
 
 export default function BootcampsProjects() {
   return (
---
+++ src/components/BootcampsProjects/techsLists.ts
@@ -0,0 +1,70 @@
+import html from '@/assets/icons/techs/HTML.svg'
+import css from '@/assets/icons/techs/CSS.svg'
+import sass from '@/assets/icons/techs/SASS.svg'
+import javascript from '@/assets/icons/techs/JAVASCRIPT.svg'
+import typescript from '@/assets/icons/techs/TYPESCRIPT.svg'
+import react from '@/assets/icons/techs/REACT.svg'
+import reactNative from '@/assets/icons/techs/REACTNATIVE.svg'
+import storybook from '@/assets/icons/techs/STORYBOOK.svg'
+import radix from '@/assets/icons/techs/RADIX.svg'
+import tailwind from '@/assets/icons/techs/TAILWIND.svg'
+import nextjs from '@/assets/icons/techs/NEXT.svg'
+import nodejs from '@/assets/icons/techs/NODE.svg'
+import express from '@/assets/icons/techs/EXPRESS.svg'
+import mongoDB from '@/assets/icons/techs/MONGO.svg'
+import prisma from '@/assets/icons/techs/PRISMA.svg'
+import docker from '@/assets/icons/techs/DOCKER.svg'
+import jest from '@/assets/icons/techs/JEST.svg'
+import graphQL from '@/assets/icons/techs/GRAPHQL.svg'
+import graphCMS from '@/assets/icons/techs/GRAPH_CMS.svg'
+import apollo from '@/assets/icons/techs/APOLLO.svg'
+import { StaticImageData } from 'next/image'
+
+export const waiterAppTechs: StaticImageData[] = [
+  html,
+  css,
+  javascript,
+  typescript,
+  react,
+  reactNative,
+  docker,
+  nodejs,
+  express,
+  mongoDB,
+]
+
+export const designSystemTechs: StaticImageData[] = [
+  html,
+  css,
+  javascript,
+  typescript,
+  react,
+  radix,
+  storybook,
+  jest,
+]
+
+export const nextBlogTechs: StaticImageData[] = [
+  html,
+  css,
+  sass,
+  javascript,
+  typescript,
+  react,
+  nextjs,
+  jest,
+  nodejs,
+  prisma,
+]
+
+export const videoPlatformTechs: StaticImageData[] = [
+  html,
+  css,
+  javascript,
+  typescript,
+  react,
+  tailwind,
+  graphQL,
+  graphCMS,
+  apollo,
+]
